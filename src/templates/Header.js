5a78c1d2d35d31701b0bf9798f13ca2424b4cbb2
Carlos Yaco <cyaco33@gmail.com>
2020-10-29 09:00:42 -0500

    implement the template

---
+++ src/templates/Header.js
@@ -0,0 +1,20 @@
+const Header =()=>{
+  const view = `
+  <div class="Header-main">
+    <div class="Header-logo">
+      <h1>
+        <a href="/">
+          1000tifi.co
+        </a>
+      </h1>
+    </div>
+  </div>  
+  <div class="Header-nav">
+    <a href="#/about/">
+      about
+    </a>
+  </div>
+  `;
+  return view;
+}
+export default Header;
