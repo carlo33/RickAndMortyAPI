e9279c1360c6c71b557a14a948679a26af332eeb
Carlos Yaco <cyaco33@gmail.com>
2020-10-29 17:58:36 -0500

    implementacion de manejo de rutas y fetch a la API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import router from './routes';
 //Con window escuchamos si la pagina termino de cargarse
 // para ejecutar una funcion
-window.addEventListener('load', router); 
+window.addEventListener('load', router);
+window.addEventListener('hashchange',router);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import Header from '../templates/Header';
 import Home from '../pages/Home';
 import Character from '../pages/Character';
-import Error from '../pages/Errro404';
+import Error404 from '../pages/Errro404';
+import getHash from '../utils/getHash';
+import resolveRoutes from '../utils/resolveRoutes';
 
 const routes = {
   '/': Home,
@@ -12,8 +14,15 @@ const routes = {
 const router = async ()=> {
   const header = null||document.getElementById('header');
   const content = null||document.getElementById('content');
-
   header.innerHTML = await Header();
+  
+  let hash = getHash();
+  console.log('hash retorna:',hash);
+  let route = await resolveRoutes(hash);
+  console.log('route retorna:',route);
+  console.log(routes[route]);
+  let render = routes[route]? routes[route] : Error404;
+  content.innerHTML = await render();
 }
 
 export default router;
